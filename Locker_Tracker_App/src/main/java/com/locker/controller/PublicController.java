e6f9507790651f4defc24719165728031764fd93
BUDAPEST\Karoly_Richard_Hollo <karoly_richard_hollo@epam.com>
2018-03-21 11:55:59 +0100

    application properties: added thymeleaf properties
    ExceptionGeneralController, PublicController, PublicService, LockerForm,
    index.html added

---
+++ Locker_Tracker_App/src/main/java/com/locker/controller/PublicController.java
@@ -0,0 +1,32 @@
+package com.locker.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.locker.form.LockerForm;
+import com.locker.service.PublicService;
+
+@Controller
+public class PublicController {
+
+private static final Logger log = LoggerFactory.getLogger(PublicController.class);
+    
+    private PublicService publicService;
+    
+    @Autowired
+    public void setPublicService(PublicService publicService) {
+        this.publicService = publicService;
+    }
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        log.info("Main url called. Returning index page...");
+        model.addAttribute("lockerForm", new LockerForm());
+        
+        return "index";
+    }
+}
